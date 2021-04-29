5b1fc531486297e98e19af626a47bb5c5fb917c1
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-04-29 15:09:29 +0530

    s

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React,{useState} from 'react'
 import './App.css';
 
 function App() {
+   //states- input query, movies
+  const [query, setQuery] = useState("step up")
 
-  const [query, setQuery] = useState("")
+  //create the state for movies, and update that state appropriate
+  const [movie, setMovie] = useState([])
 
-  function SearchMovie(){
-    console.log("hello");
+  const [test] = useState([3,5,6,3,3]);
+  const [test1] = useState([{num:3},{num:3},{num:4},{num:9},{num:5}]);
+
+  async function SearchMovie(e){
+    e.preventDefault();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=2134f52591bc943264d33b4bd3b203c9&language=en-US&query=${query}&page=1&include_adult=false`;
+    
+    try{
+      const pro = await fetch(url);
+      const res = await pro.json();
+      console.log(res);
+      setMovie(res.results);
+    }catch(err){
+      console.error(err);
+    }
   }
 
+  console.log(movie)
+  console.log(test1)
+
+  // const exa = !query ? "none" : "block";
   return (
+    <>
         <form className="form" onSubmit={SearchMovie}>
             <label className="label" htmlFor="query">
               Movie Name
@@ -23,6 +44,30 @@ function App() {
             </label>
             <button className="button" type="submit">Search</button>
         </form>
+
+        <div>
+              <h1 style={{textAlign:"center", display: !query ? "none" : "block" }}><small>you might be searching for : {query}</small></h1>
+        </div>
+
+
+        {/* {movies.filter(movie => movie.poster_path).map(movie => ( */}
+                   
+          <div className="card-list">
+             <p className="test">
+                {test.map(test => test + 10)}
+             </p>
+             
+             <p className="test">
+                {test1.map(test => test.num )}
+             </p>
+             
+             <div className="test">
+                {movie.map(test => {
+                  return (<p key={test.id}>{test.id}</p>)
+                  })}
+             </div>
+          </div>    
+      </>
   );
 }
 
