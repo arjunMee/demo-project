a911ceef355ac2f876617e51b30949d8449f01d6
arjunMee <arjun.bulli12@gmail.com>
2021-06-29 20:49:04 +0530

    yg

--- movie_search/src/App.js
+++ movie_search/src/App.js
@@ -1,60 +1,16 @@
-import React,{useState} from 'react'
-import './App.css';
-import MovieSearch from './movieSearch'
+import React from 'react'
+import './Home.css'
 
-function App() {
-   //states- input query, movies
-  const [query, setQuery] = useState("step up")
-
-  //create the state for movies, and update that state appropriate
-  const [movie, setMovie] = useState([])
-
-  // const [test] = useState([3,5,6,3,3]);
-  // const [test1] = useState([{num:3},{num:3},{num:4},{num:9},{num:5}]);
-
-  async function SearchMovie(e){
-    e.preventDefault();
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=2134f52591bc943264d33b4bd3b203c9&language=en-US&query=${query}&page=1&include_adult=false`;
-    
-    try{
-      const pro = await fetch(url);
-      const res = await pro.json();
-      console.log(res);
-      setMovie(res.results);
-    }catch(err){
-      console.error(err);
-    }
-  }
-
-  // console.log(movie)
-  // console.log(test1)
-
-  // const exa = !query ? "none" : "block";
-
+const Home = () => {
   return (
-    <>
-        <form className="form" onSubmit={SearchMovie}>
-            <label className="label" htmlFor="query">
-              Movie Name
-              <input 
-                className="input"
-                type="text"
-                name="query"
-                placeholder="e.g. Step Up"
-                value={query}
-                onChange={e => setQuery(e.target.value)}/>
-            </label>
-            <button className="button" type="submit">Search</button>
-        </form>
-
-        <div>
-              <h1 style={{textAlign:"center", display: !query ? "none" : "block" }}><small>you might be searching for : {query}</small></h1>
-        </div>
-
-        <MovieSearch movie={movie}/>
-
-      </>
-  );
+    <div className='home'>
+      <img
+        src='https://images-eu.ssl-images-amazon.com/images/G/31/img18/HPC/GW/EES_Aug_1500x600._CB405921824_.jpg'
+        alt='homie'
+        className='posterImage'
+      />
+    </div>
+  )
 }
 
-export default App;
+export default Home
